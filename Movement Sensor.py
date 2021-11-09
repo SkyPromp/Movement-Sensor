2205fd75fe61c2d9ebce02aa81dbb9372c6b1e25
Max Poppe <max.poppe@ugent.be>
2021-11-09 18:14:24 +0100

    added functionality

---
+++ Movement Sensor.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import winsound as ws
+
+video = cv.VideoCapture(0)
+_, prevframe = video.read()
+
+while video.isOpened():
+    ret, frame = video.read()
+
+    if ret:
+        cv.imshow("frame", frame)
+        cv.imshow("prevframe", prevframe)
+        if cv.matchTemplate(prevframe, frame, cv.TM_CCOEFF_NORMED)[0][0] < 0.97:
+            print("movement")
+            ws.Beep(440, 200)
+        if cv.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+    prevframe = frame
+
+video.release()
+cv.destroyAllWindows()
